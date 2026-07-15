0b5daeac03d35c0ac5ccd674691225ec01e20814
clllynnn <itslynn0927@gmail.com>
2026-07-15 14:09:30 +0800

    Initial Commit

---
+++ Snoots!/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snoots!
+//
+//  Created by lynn on 2026/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snoots!/Snoots_App.swift
@@ -0,0 +1,17 @@
+//
+//  Snoots_App.swift
+//  Snoots!
+//
+//  Created by lynn on 2026/7/15.
+//
+
+import SwiftUI
+
+@main
+struct Snoots_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
